14e95a947fd1009205ef6d3fe4ce1bfecf9eb93f
Muaiyid AlZadjali <muaidkhalil999@gmail.com>
2026-05-10 00:13:36 +0400

    changes done to work with the about us page .....

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Profile from "./components/Profile";
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} /> {/* ✅ important */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
