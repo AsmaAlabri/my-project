142ff3dff07c2ea4e5222d0c5572afda6237df24
Muaiyid AlZadjali <muaidkhalil999@gmail.com>
2026-05-16 19:09:59 +0400

    Refactor routes in App component
    Refactored routes in App component to include dynamic routes for cats and dogs, and added comments for better organization.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,70 @@
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Profile from "./components/Profile";
+
 import BirdList from "./components/BirdList";
 import CatList from "./components/CatList";
 import DogList from "./components/DogList";
+
 import PersianCat from "./cats/PersianCat";
-import DomesticCat from "./cats/DomesticCat"
+import DomesticCat from "./cats/DomesticCat";
 import SiameseCat from "./cats/SiameseCat";
+
 import FrenchDog from "./dogs/FrenchDog";
 import GermenDog from "./dogs/GermenDog";
 import GoldenDog from "./dogs/GoldenDog";
+
 import PeacockBird from "./birds/PeacockBird";
 import ParrotBird from "./birds/ParrotBird";
 import FlamingoBird from "./birds/FlamingoBird";
+
 import AdminDashboard from "./components/AdminDashboard";
-import AdminLogin from "./components/Adminlogin";
+import AdminLogin from "./components/AdminLogin";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* USER ROUTES */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/profile" element={<Profile />} />
+
+        {/* LIST PAGES */}
         <Route path="/CatList" element={<CatList />} />
         <Route path="/DogList" element={<DogList />} />
         <Route path="/BirdList" element={<BirdList />} />
-        <Route path="/PersianCat" element={<PersianCat/>} />
-        <Route path="/DomesticCat" element={<DomesticCat/>} />
-        <Route path="/SiameseCat" element={<SiameseCat/>} />
-        <Route path="/FrenchDog" element={<FrenchDog/>}/>
-        <Route path="/GermenDog" element={<GermenDog/>}/>
-        <Route path="/GoldenDog" element={<GoldenDog/>}/>
-        <Route path="/PeacockBird" element={<PeacockBird/>}/>
-        <Route path="/ParrotBird" element={<ParrotBird/>}/>
-        <Route path="/FlamingoBird" element={<FlamingoBird/>}/>
-        <Route path="/admin" element={<AdminDashboard />} />
+
+        {/* STATIC CAT PAGES */}
+        <Route path="/PersianCat" element={<PersianCat />} />
+        <Route path="/DomesticCat" element={<DomesticCat />} />
+        <Route path="/SiameseCat" element={<SiameseCat />} />
+
+        {/* ✅ DYNAMIC CAT ROUTE FROM MONGODB */}
+        <Route path="/cats/:id" element={<PersianCat />} />
+
+        {/* DOG PAGES */}
+        <Route path="/dogs/:id" element={<GermenDog />} />
+
+        {/* BIRD PAGES */}
+        <Route path="/dogs/:id" element={<FlamingoBird />} />
+
+
+
+        {/* ADMIN ROUTES */}
         <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin"       element={<AdminDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+
       </Routes>
     </BrowserRouter>
   );
